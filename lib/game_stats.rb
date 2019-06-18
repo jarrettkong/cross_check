f9f17e607898141e037e11d948da0a7c1aed72c9
Jarrett Kong <jarrettkong@me.com>
2019-06-18 11:38:52 -0400

    Fix rounding errors

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -15,7 +15,7 @@ module GameStats
 
   def biggest_blowout
     @games.map do |game|
-      (game['home_goals'].to_i + game['away_goals'].to_i).abs
+      (game['home_goals'].to_i - game['away_goals'].to_i).abs
     end.max
   end
 
@@ -39,19 +39,17 @@ module GameStats
   end
 
   def average_goals_per_game
-    @games.inject(0) do |acc, game|
+    (@games.inject(0) do |acc, game|
       acc + game['home_goals'].to_i + game['away_goals'].to_i
-    end / @games.length.to_f
+    end / @games.length.to_f).round(2)
   end
 
   def average_goals_by_season
-    total_goals = @games.each_with_object({}) do |game, acc|
+    @games.inject({}) do |acc, game|
       acc[game['season']] = 0 unless acc[game['season']]
       acc[game['season']] += game['home_goals'].to_i + game['away_goals'].to_i
-    end
-
-    total_goals.map do |season, goals|
-      [season, goals.to_f / (@games.select { |game| game['season'] == season }).length]
+    end.map do |season, goals|
+      [season, (goals.to_f / (@games.select { |game| game['season'] == season }).length).round(2)]
     end.to_h
    end
 end
