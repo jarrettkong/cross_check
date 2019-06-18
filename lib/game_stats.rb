b8abfa1d8dbdf5c01f110df517ad7c0c8a532e4b
Jarrett Kong <jarrettkong@me.com>
2019-06-18 10:45:06 -0400

    add biggest blowout, difference, percent wins, and count methods

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -1,38 +1,45 @@
+# frozen_string_literal: true
+
 module GameStats
   def highest_total_score
     @games.map do |game|
-      game["home_goals"].to_i + game["away_goals"].to_i
+      game['home_goals'].to_i + game['away_goals'].to_i
     end.max
   end
 
   def lowest_total_score
     @games.map do |game|
-      game["home_goals"].to_i + game["away_goals"].to_i
+      game['home_goals'].to_i + game['away_goals'].to_i
     end.min
   end
 
   def biggest_blowout
-    
+    @games.map do |game|
+      (game['home_goals'].to_i + game['away_goals'].to_i).abs
+    end.max
   end
 
   def percentage_home_wins
-    
+    @games.select do |game|
+      game['outcome'].include?('home win')
+    end.length.to_f / @games.length
   end
 
   def percentage_visitor_wins
-    
+    @games.select do |game|
+      game['outcome'].include?('away win')
+    end.length.to_f / @games.length
   end
 
   def count_of_games_by_season
-    
+    @games.inject({}) do |acc, game|
+      acc[game['season']] = 0 unless acc[game['season']]
+      acc[game['season']] += 1
+      acc
+    end
   end
 
-  def average_goals_per_game
-    
-  end
+  def average_goals_per_game; end
 
-  def average_goals_by_season
-    
-  end
-
+  def average_goals_by_season; end
 end
